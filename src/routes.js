3f70d8496b3d0dddd4ab72bacb36455f5978fbe3
Angelo Marzari Possatti <angelopossatti@gmail.com>
2019-10-26 21:23:24 -0300

    Import way has been updated by using Sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'Olá!' });
+    return res.json({ message: 'Olá rockets!' });
 });
 
-module.exports = routes;
+export default routes;
