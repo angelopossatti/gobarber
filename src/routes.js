2bb92f9a6519972fec106b8ee012b05e49fd1669
Angelo Marzari Possatti <angelopossatti@gmail.com>
2019-12-11 23:10:48 -0300

    Create appointment route and controller to save an appointment.

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -16,8 +17,11 @@ routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
 routes.use(authMiddleware); // middleware to next routes (below)
+
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
+
 routes.get('/providers', ProviderController.index);
 
 export default routes;
