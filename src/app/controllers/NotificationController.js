45060b96afdc69ce1c5eca562e31373b4990b335
Angelo Marzari Possatti <angelopossatti@gmail.com>
2020-02-27 15:37:12 -0300

    Create the provider notification list

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,29 @@
+import Notification from '../schemas/Notification';
+import User from '../models/User';
+
+class NotificationController {
+  async index(req, res) {
+    /**
+     * Check if provider_id is a provider.
+     */
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Acesso restrito a fornecedores de serviço.' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort('createdAt')
+      .limit(20);
+
+    return res.json(notifications);
+  }
+}
+
+export default new NotificationController();
