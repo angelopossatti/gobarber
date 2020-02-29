3cc676f6768ec9b97512b4df3e2f4b8e97886f53
Angelo Marzari Possatti <angelopossatti@gmail.com>
2020-02-28 23:26:55 -0300

    Add feature to send mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7bf2aecfc9a37a',
+    pass: 'bff03976734a7a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
